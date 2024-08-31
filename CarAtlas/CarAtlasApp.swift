495860b17dec9978bac03dfec14f31defce4c26e
Marie Metay <mariemetay.pro@gmail.com>
2024-08-31 16:00:27 +0200

    Initial Commit

---
+++ CarAtlas/CarAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAtlasApp.swift
+//  CarAtlas
+//
+//  Created by Marie M on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
